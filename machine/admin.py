aecbec0f42acbbb90a46586c1471bee205f74561
eiffelfly <eric525282@gmail.com>
2020-07-24 14:53:56 +0800

    Major:Admin - registered machine admin

--- machine/admin.py
+++ machine/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Machine
 
 # Register your models here.
+class MachineAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+     # We don't display all the application description
+    def description_shortcut(self, obj):
+        if len(obj.description) > 32:
+            return f'{obj.description[:32]} ...'
+        else:
+            return obj.description
+
+admin.site.register(Machine, MachineAdmin)
