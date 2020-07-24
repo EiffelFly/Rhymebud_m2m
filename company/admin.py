b1760a7b341bf150ef1593ab108b3875249b86da
eiffelfly <eric525282@gmail.com>
2020-07-24 14:44:29 +0800

    Major:Admin - Company Admin

--- company/admin.py
+++ company/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Company
 
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'company_website']
+    filter_horizontal = ['machine', 'application']
 
+    # We don't display all the company_website
+    def from_url_shortcut(self, obj):
+        if len(obj.from_url) > 64:
+            return f'{obj.from_url[:64]} ...'
+        else:
+            return obj.from_url
+
+
+admin.site.register(Company, CompanyAdmin)
